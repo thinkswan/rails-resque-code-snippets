0c18be140e916166947f62391a4f07dd155f1334
Graham Swan <thinkswan@gmail.com>
2015-07-10 00:35:26 +0700

    Add validators to Snippet model

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,2 +1,7 @@
 class Snippet < ActiveRecord::Base
+  enum language: [ :ruby, :javascript ]
+
+  validates :title, presence: true
+  validates :language, presence: true
+  validates :content, presence: true
 end
