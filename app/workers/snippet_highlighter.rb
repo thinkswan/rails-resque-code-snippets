55601f7e9f2ba486bf32c1452398386762761d0f
Graham Swan <thinkswan@gmail.com>
2015-07-10 19:47:15 +0700

    Move API logic from worker to model

--- app/workers/snippet_highlighter.rb
+++ app/workers/snippet_highlighter.rb
@@ -1,21 +1,11 @@
 class SnippetHighlighter
   @queue = :snippets_queue
 
-  API_LANGUAGES = {
-    ruby: 'Ruby',
-    python: 'Python',
-    javascript: 'JavaScript'
-  }.freeze
-
   def self.perform(snippet_id)
     snippet = Snippet.find(snippet_id)
 
-    data = {
-      language: API_LANGUAGES[snippet.language.to_sym],
-      source: snippet.plain_code,
-      theme: 'Dawn'
-    }
-    uri = URI.parse('http://markup.su/api/highlighter')
+    uri = URI.parse(Snippet::API_URI)
+    data = snippet.api_post_data
     request = Net::HTTP.post_form(uri, data)
     snippet.update_attribute(:highlighted_code, request.body)
   end
