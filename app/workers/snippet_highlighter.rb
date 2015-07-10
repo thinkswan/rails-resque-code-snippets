1685b78b854548b07eae552723670c3e6dfe59d8
Graham Swan <thinkswan@gmail.com>
2015-07-10 14:51:02 +0700

    Handle API languages properly

--- app/workers/snippet_highlighter.rb
+++ app/workers/snippet_highlighter.rb
@@ -1,11 +1,21 @@
 class SnippetHighlighter
   @queue = :snippets_queue
 
+  API_LANGUAGES = {
+    ruby: 'Ruby',
+    javascript: 'JavaScript'
+  }.freeze
+
   def self.perform(snippet_id)
     snippet = Snippet.find(snippet_id)
 
+    data = {
+      language: API_LANGUAGES[snippet.language.to_sym],
+      source: snippet.plain_code,
+      theme: 'Dawn'
+    }
     uri = URI.parse('http://markup.su/api/highlighter')
-    request = Net::HTTP.post_form(uri, { language: snippet.language.humanize, source: snippet.plain_code, theme: 'Dawn' })
+    request = Net::HTTP.post_form(uri, data)
     snippet.update_attribute(:highlighted_code, request.body)
   end
 end
